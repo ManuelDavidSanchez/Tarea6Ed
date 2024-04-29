a90559c391b20ea060d24182f9d268b2359d7ddb
ManuelDavidSanchez <168478205+ManuelDavidSanchez@users.noreply.github.com>
2024-04-29 21:43:14 +0200

    Add files via upload

---
+++ Tarea6ED/src/MainClass.java
@@ -0,0 +1,12 @@
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        AplicacionDescuentoCarrito miCarrito = new AplicacionDescuentoCarrito();
+
+        miCarrito.aplicarDescuento(100, 5);
+
+    }
+
+}
