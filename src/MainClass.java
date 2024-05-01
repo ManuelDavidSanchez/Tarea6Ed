979fb02819121fde9db7435ea52ed53ccc201172
ManuelDavidSanchezQuiñones <davidsqo@gmail.com>
2024-05-01 11:41:48 +0200

    Esta es la 1º version de la app.

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,9 +2,10 @@
 public class MainClass {
 
     public static void main(String[] args) {
-
+        
+        //Creamos un carrito nuevo.
         AplicacionDescuentoCarrito miCarrito = new AplicacionDescuentoCarrito();
-
+        //aplicamos metodo al carrito creado.
         miCarrito.aplicarDescuento(100, 5);
 
     }
